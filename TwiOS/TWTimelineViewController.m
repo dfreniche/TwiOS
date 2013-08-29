1803cb28902cba44809d90322e17258a31b299e7
Diego Freniche Brito <diego@freniche.com>
2013-08-29 06:10:45 +0200

    Added code to get timeline, printing JSON data to console

--- TwiOS/TWTimelineViewController.m
+++ TwiOS/TWTimelineViewController.m
@@ -8,6 +8,7 @@
 
 #import "TWTimelineViewController.h"
 #import <Social/Social.h>
+#import <Accounts/Accounts.h>
 
 @interface TWTimelineViewController ()
 
@@ -19,6 +20,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self retrieveTweets:nil];
 }
 
 - (void)didReceiveMemoryWarning
@@ -37,4 +39,75 @@
     //}
     
 }
+
+// Method 4: Retrieving The User's Home Timeline. This uses v1.1 of the Twitter API. The v1.1 API link is included but commented out.
+- (IBAction)retrieveTweets:(id)sender
+{
+    // Create an account store
+    ACAccountStore *twitter = [[ACAccountStore alloc] init];
+    
+    // Create an account type
+    ACAccountType *twAccountType = [twitter accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierTwitter];
+    
+    // Request Access to the twitter account
+    [twitter requestAccessToAccountsWithType:twAccountType options:nil completion:^(BOOL granted, NSError *error)
+     {
+         if (granted)
+         {
+             // Create an Account
+             ACAccount *twAccount = [[ACAccount alloc] initWithAccountType:twAccountType];
+             NSArray *accounts = [twitter accountsWithAccountType:twAccountType];
+             twAccount = [accounts lastObject];
+             
+             // Version 1.1 of the Twitter API only supports JSON responses.
+             // Create an NSURL instance variable that points to the home_timeline end point.
+             NSURL *twitterURL = [[NSURL alloc] initWithString:@"https://api.twitter.com/1.1/statuses/home_timeline.json"];
+             
+             // Version 1.0 of the Twiter API supports XML responses.
+             // Use this URL if you want to see an XML response.
+             //NSURL *twitterURL2 = [[NSURL alloc] initWithString:@"http://api.twitter.com/1/statuses/home_timeline.xml"];
+             
+             // Create a request
+             SLRequest *requestUsersTweets = [SLRequest requestForServiceType:SLServiceTypeTwitter
+                                                                requestMethod:SLRequestMethodGET
+                                                                          URL:twitterURL
+                                                                   parameters:nil];
+             
+             // Set the account to be used with the request
+             [requestUsersTweets setAccount:twAccount];
+             
+             // Perform the request
+             [requestUsersTweets performRequestWithHandler:^(NSData *responseData, NSHTTPURLResponse *urlResponse, NSError *error2)
+              {
+                  // The output of the request is placed in the log.
+                  NSLog(@"HTTP Response: %i", [urlResponse statusCode]);
+                  // The output of the request is placed in the log.
+                  NSDictionary *jsonResponse = [NSJSONSerialization JSONObjectWithData:responseData options:0 error:nil];
+                  
+                  NSLog(@"%@", jsonResponse);
+                  
+              }];
+             
+             // Tidy Up
+             twAccount = nil;
+             accounts = nil;
+             twitterURL = nil;
+             requestUsersTweets = nil;
+         }
+         
+         // If permission is not granted to use the Twitter account...
+         
+         else
+             
+         {
+             NSLog(@"Permission Not Granted");
+             NSLog(@"Error: %@", error);
+         }
+     }];
+    
+    // Tidy up
+    twitter = nil;
+    twAccountType = nil;
+}
+
 @end
